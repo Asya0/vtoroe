4de2239470325212f1845a19912334e9bb237a87
Asya0 <anya.gerasimenko763@mail.ru>
2024-05-09 16:08:33 +0600

    feat: added yandex map

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,35 @@
 import "../scss/style.scss";
 import "./slider";
+
+// function init() {
+//   const ymaps = window.ymaps;
+//   let map = new ymaps.Map(
+//     "map-test",
+//     {
+//       center: [55.684013520979256, 37.55043825396722],
+//       zoom: 17,
+//     },
+//     {
+//       searchControlProvider: "yandex#search",
+//     }
+//   );
+// }
+
+// ymaps.ready(init);
+
+async function initMap() {
+  await ymaps3.ready;
+
+  const { YMap, YMapDefaultSchemeLayer } = ymaps3;
+
+  const map = new YMap(document.getElementById("map-test"), {
+    location: {
+      center: [37.588144, 55.733842],
+      zoom: 10,
+    },
+  });
+
+  map.addChild(new YMapDefaultSchemeLayer());
+}
+
+initMap();
